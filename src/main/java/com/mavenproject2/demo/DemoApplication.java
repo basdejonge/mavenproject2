1318939dfc3fcc3562566d5bcce087192054b9e0
Bas de Jonge <bas.de.jonge@heroes-corp.com>
2020-10-15 11:09:25 +0200

    Update DemoApplication.java

--- src/main/java/com/mavenproject2/demo/DemoApplication.java
+++ src/main/java/com/mavenproject2/demo/DemoApplication.java
@@ -1,7 +1,6 @@
 package com.mavenproject2.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,9 +9,4 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-}
-@Service
-	int sum(int a, int b) {
-		return a + b;
-	}
 }
