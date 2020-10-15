04035d59bdf5ab18397805e1830dec62f4f4756e
Bas de Jonge <bas.de.jonge@heroes-corp.com>
2020-10-15 10:07:17 +0200

    Create CalculatorController.java

---
+++ src/main/java/com/mavenproject2/demo/CalculatorController.java
@@ -0,0 +1,20 @@
+package com.mavenproject2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+@Service
+public class Calculator {
+	int sum(int a, int b) {
+		return a + b;
+	}
+}
