1b3e41fe074b7249046bbe700601f1fe3f408fbf
Bas de Jonge <bas.de.jonge@heroes-corp.com>
2020-10-15 10:08:25 +0200

    Update CalculatorController.java

--- src/main/java/com/mavenproject2/demo/CalculatorController.java
+++ src/main/java/com/mavenproject2/demo/CalculatorController.java
@@ -1,20 +1,16 @@
 package com.mavenproject2.demo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
-
-@SpringBootApplication
-public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
-}
-@Service
-public class Calculator {
-	int sum(int a, int b) {
-		return a + b;
-	}
+@RestController
+class CalculatorController {
+    @Autowired
+    private Calculator calculator;
+    @RequestMapping("/sum")
+    String sum(@RequestParam("a") Integer a,
+               @RequestParam("b") Integer b) {
+        return String.valueOf(calculator.sum(a, b));
+    }
 }
